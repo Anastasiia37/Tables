6bd8153876d2a2d18a3fc22e4a65240d62bbd812
Anastasiia37 <43875353+Anastasiia37@users.noreply.github.com>
2018-11-09 12:22:48 +0200

    Add files via upload

---
+++ AppointmentTypes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AppointmentTypes]
+(
+	[AppointmentTypeId] INT NOT NULL IDENTITY(1, 1),
+	[Name] NVARCHAR(20) NOT NULL,
+
+	CONSTRAINT PK_AppointmentTypes PRIMARY KEY CLUSTERED ( [AppointmentTypeId] ),
+)
---
+++ Bills.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Bills]
+(
+	[BillId] INT NOT NULL IDENTITY(1, 1),
+	[InvoiceNumber] UNIQUEIDENTIFIER NOT NULL DEFAULT ( NEWID() ),
+	[CreationDate] DATETIME NOT NULL DEFAULT ( GETDATE() ),
+	[Amount] SMALLMONEY NOT NULL,
+	[AppointmentId] INT NOT NULL,
+	[Completed] BIT NOT NULL DEFAULT 0,
+	[IsDeleted] BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT PK_Bills PRIMARY KEY CLUSTERED ( [BillId] ),
+	CONSTRAINT FK_Bills_Appointments FOREIGN KEY ( [AppointmentId] ) REFERENCES [dbo].[Appointments]( [AppointmentId] ),
+	CONSTRAINT UC_InvoiceNumber UNIQUE ( [InvoiceNumber] ),
+	CONSTRAINT CHK_Amount CHECK ( [Amount] >= 0 )
+)
